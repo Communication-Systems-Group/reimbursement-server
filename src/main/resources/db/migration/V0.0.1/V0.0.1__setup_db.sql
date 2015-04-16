592247cc4f4871cb85e963cc5abd932d243b7fad
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-04-16 22:13:11 +0200

    add constraints to initial script

--- src/main/resources/db/migration/V0.0.1/V0.0.1__setup_db.sql
+++ src/main/resources/db/migration/V0.0.1/V0.0.1__setup_db.sql
@@ -1,6 +1,8 @@
 CREATE TABLE User (
-	id int(10) auto_increment NOT NULL,
+	id int(10) auto_increment NOT NULL PRIMARY KEY,
 	uid varchar NOT NULL,
 	first_name varchar NOT NULL,
 	last_name varchar NOT NULL
 );
+
+ALTER TABLE User ADD CONSTRAINT UID_UNIQUE UNIQUE(UID);
