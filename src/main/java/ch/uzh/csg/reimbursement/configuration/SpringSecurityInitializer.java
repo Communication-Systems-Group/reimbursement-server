5d74d5efa4227b2e304d93309dd831b639f39437
Christian Davatz <crixx@davatz.eu>
2015-05-16 22:56:41 +0200

    Configured MultipartFilter to be applied before the SecurityFilter in order to preserve multipartfile uploads without csrf

--- src/main/java/ch/uzh/csg/reimbursement/configuration/SpringSecurityInitializer.java
+++ src/main/java/ch/uzh/csg/reimbursement/configuration/SpringSecurityInitializer.java
@@ -1,11 +1,19 @@
 package ch.uzh.csg.reimbursement.configuration;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 	public SpringSecurityInitializer() {
 		super(WebSecurityConfiguration.class);
+	}
+
+	@Override
+	protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+		insertFilters(servletContext, new MultipartFilter());
 	}
 
 }
