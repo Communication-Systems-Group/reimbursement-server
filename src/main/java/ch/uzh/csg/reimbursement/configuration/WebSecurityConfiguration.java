ffbbd7b506301be33e5127e866e6ebb7bc6c0235
Christian Davatz <crixx@davatz.eu>
2015-05-17 22:01:30 +0200

    Added fixed adding of csrf tokens in cookies

--- src/main/java/ch/uzh/csg/reimbursement/configuration/WebSecurityConfiguration.java
+++ src/main/java/ch/uzh/csg/reimbursement/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,14 @@
 package ch.uzh.csg.reimbursement.configuration;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +19,12 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import ch.uzh.csg.reimbursement.security.FormLoginFailureHandler;
 import ch.uzh.csg.reimbursement.security.FormLoginSuccessHandler;
@@ -39,10 +54,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-		//TODO should we use the cookie or the header based csrf filter approach? for angular probably better cookie
-		//.addFilterAfter(new CsrfTokenBindingFilter(), CsrfFilter.class)
-		.csrf().disable()
-		//		.addFilterAfter(new StatelessCsrfFilter(), CsrfFilter.class)
+		.csrf()
+		.csrfTokenRepository(csrfTokenRepository())
+		.and()
+		.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+
+		http
 		.exceptionHandling()
 		.authenticationEntryPoint(authenticationEntryPoint)
 		.and().authorizeRequests()
@@ -64,6 +81,35 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.and()
 		.sessionManagement()
 		.maximumSessions(1);
+	}
+
+	private Filter csrfHeaderFilter() {
+		return new OncePerRequestFilter() {
+			@Override
+			protected void doFilterInternal(HttpServletRequest request,
+					HttpServletResponse response, FilterChain filterChain)
+							throws ServletException, IOException {
+				CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+						.getName());
+				if (csrf != null) {
+					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+					String token = csrf.getToken();
+					if (cookie == null || token != null
+							&& !token.equals(cookie.getValue())) {
+						cookie = new Cookie("XSRF-TOKEN", token);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+				filterChain.doFilter(request, response);
+			}
+		};
+	}
+
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
 	}
 
 	@Configuration
