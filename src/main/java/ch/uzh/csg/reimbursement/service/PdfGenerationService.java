685fa2079cf1c0b6e5a682682ae618be24382520
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-09-30 16:51:05 +0200

    remove workingDirectory from application.properties, include files with spring, consolidate exceptions in pdf generation

--- src/main/java/ch/uzh/csg/reimbursement/service/PdfGenerationService.java
+++ src/main/java/ch/uzh/csg/reimbursement/service/PdfGenerationService.java
@@ -1,13 +1,14 @@
 package ch.uzh.csg.reimbursement.service;
 
+import static org.springframework.util.ResourceUtils.getFile;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
@@ -16,15 +17,19 @@ import javax.xml.transform.stream.StreamSource;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.xml.sax.SAXException;
 
 import ch.uzh.csg.reimbursement.model.Expense;
 import ch.uzh.csg.reimbursement.model.ExpensePdf;
-import ch.uzh.csg.reimbursement.utils.PropertyProvider;
+import ch.uzh.csg.reimbursement.model.exception.ServiceException;
 
 @Service
 public class PdfGenerationService {
+
+	private final Logger LOG = LoggerFactory.getLogger(PdfGenerationService.class);
 
 	private FopFactory fopFactory;
 	private TransformerFactory tFactory = TransformerFactory.newInstance();
@@ -33,14 +38,21 @@ public class PdfGenerationService {
 		ExpensePdf response = expense.getExpensePdf();
 
 		// Define filepaths
-		String rootPath = PropertyProvider.INSTANCE.getProperty("reimbursement.workingDirectory");
-		String xconfFile = rootPath + "src/main/resources/fop.xconf";
-		String xslFile = rootPath + "src/main/resources/foo-xml2fo.xsl";
-		String xmlFile = rootPath + "src/main/resources/foo.xml";
+		File xslFile = null;
+		File xmlFile = null;
+
+		try {
+			xslFile = getFile("classpath:foo-xml2fo.xsl");
+			xmlFile = getFile("classpath:foo.xml");
+
+		} catch (FileNotFoundException e) {
+			LOG.error("PDF source file is missing.");
+			throw new ServiceException();
+		}
 
 		try {
 			// Initialize fopfactory and load the necessary xconf file
-			fopFactory = FopFactory.newInstance(new File(xconfFile));
+			fopFactory = FopFactory.newInstance(new File(".").toURI());
 
 			// Setup buffer to obtain the content length
 			ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -49,36 +61,26 @@ public class PdfGenerationService {
 			Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
 
 			// Setup Transformer
-			Source xsltSrc = new StreamSource(new File(xslFile));
+			Source xsltSrc = new StreamSource(xslFile);
 			Transformer transformer = tFactory.newTransformer(xsltSrc);
 
 			// Make sure the XSL transformation's result is piped through to FOP
 			Result res = new SAXResult(fop.getDefaultHandler());
 
 			// Setup input
-			Source src = new StreamSource(new File(xmlFile));
+			Source src = new StreamSource(xmlFile);
 
 			//Start the transformation and rendering process
 			transformer.transform(src, res);
 
 			// Store the result in the response object ExpensePdf
 			response = new ExpensePdf("application/pdf",out.size(),out.toByteArray());
-		} catch(IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TransformerException e) {
+		} catch (SAXException | TransformerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return response;
 	}
-
 
 }
