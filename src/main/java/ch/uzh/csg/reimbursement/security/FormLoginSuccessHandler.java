e49eba8cf893a0fc9bc65719f76767f927247c30
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-10-21 11:24:56 +0200

    do not show internal login data on login

--- src/main/java/ch/uzh/csg/reimbursement/security/FormLoginSuccessHandler.java
+++ src/main/java/ch/uzh/csg/reimbursement/security/FormLoginSuccessHandler.java
@@ -1,7 +1,6 @@
 package ch.uzh.csg.reimbursement.security;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -9,32 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class FormLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FormLoginSuccessHandler.class);
 
-	private final ObjectMapper mapper;
-
-	@Autowired
-	FormLoginSuccessHandler(ObjectMapper mapper) {
-		this.mapper = mapper;
-	}
-
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		response.setStatus(HttpServletResponse.SC_OK);
-
-		PrintWriter writer = response.getWriter();
-		mapper.writeValue(writer, authentication.getPrincipal());
-		writer.flush();
 
 		LOGGER.info("Authentication success:" + authentication.getName());
 	}
