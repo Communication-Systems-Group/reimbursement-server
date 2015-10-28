a89acfe1873fd009512d6ebd0e9d9754eec0f7c5
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-10-28 11:28:16 +0100

    CTRL + SHIFT + O, @crixx

--- src/main/java/ch/uzh/csg/reimbursement/mail/SimpleEmailService.java
+++ src/main/java/ch/uzh/csg/reimbursement/mail/SimpleEmailService.java
@@ -1,26 +1,17 @@
 package ch.uzh.csg.reimbursement.mail;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import javax.mail.internet.MimeMessage;
 
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.velocity.VelocityEngineUtils;
-
-import ch.uzh.csg.reimbursement.model.Role;
-import ch.uzh.csg.reimbursement.model.User;
 
 @Service
 public class SimpleEmailService {
@@ -45,18 +36,19 @@ public class SimpleEmailService {
 
 	private void sendConfirmationEmail() {
 		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+			@Override
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
-//				message.setTo("sebschrepfer@hotmail.com");
-//				message.setCc("davatzc@gmail.com");
+				//				message.setTo("sebschrepfer@hotmail.com");
+				//				message.setCc("davatzc@gmail.com");
 				message.setTo("crixx@davatz.eu");
 				message.setFrom("bocek@ifi.uzh.ch");
 				message.setSubject("[reimbursement] New expense item");
-				
+
 				Map<String, Object> model = new HashMap<String, Object>();
 				model.put("firstname", "Sebastian");
 				model.put("lastname", "Schrepfer");
-				
+
 				String text = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "ch/uzh/csg/reimbursement/mail/email-template.vm", "utf-8", model);
 				message.setText(text, true);
 			}
