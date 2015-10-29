97548b4d0fae6dbf4a8a77b35c01fb7e971ea4c8
Sebastian Schrepfer <sebschrepfer@hotmail.com>
2015-10-29 16:38:24 +0100

    CTRL + SHIFT + O, @crixx :wink:

--- src/main/java/ch/uzh/csg/reimbursement/service/EmailService.java
+++ src/main/java/ch/uzh/csg/reimbursement/service/EmailService.java
@@ -1,8 +1,5 @@
 package ch.uzh.csg.reimbursement.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.mail.internet.MimeMessage;
 
 import org.apache.velocity.app.VelocityEngine;
@@ -23,27 +20,27 @@ public class EmailService {
 
 	@Autowired
 	private VelocityEngine velocityEngine;
-	
+
 	private String noAddress = "noAddress";
-	
+
 	//TODO check if Emailvalidation is needed!
 	public void sendEmail(final String fromEmail, final String fromName, final String to, final String subject, Templateinformation templateInfo) {
 		sendEmail(fromEmail, fromName,to, subject, noAddress, noAddress, noAddress, templateInfo);
 	}
-	
+
 	public void sendEmail(final String fromEmail,final String fromName, final String toEmail, final String subject, final String ccEmail, final String bccEmail, final String replyToEmail,final Templateinformation templateInfo) {
 		MimeMessagePreparator preparator = new MimeMessagePreparator() {
 			@Override
 			public void prepare(MimeMessage mimeMessage) throws Exception {
 				MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
 				message.setFrom(fromEmail, fromName);
-				message.setTo(toEmail);				
+				message.setTo(toEmail);
 				if (!ccEmail.equals(noAddress)){
-					message.setCc(ccEmail);		
+					message.setCc(ccEmail);
 				}
 				if (!bccEmail.equals(noAddress)){
-					message.setBcc(bccEmail);				
-								}
+					message.setBcc(bccEmail);
+				}
 				if (!replyToEmail.equals(noAddress)){
 					message.setReplyTo(replyToEmail);
 				}
