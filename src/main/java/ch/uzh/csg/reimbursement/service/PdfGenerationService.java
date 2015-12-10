7600cab5ec2615c048337a7e53e607339c7a5133
Robin <mail@xorox.ch>
2015-12-10 14:50:51 +0100

    Removed attachment cover page and replace with simple receipt number.

--- src/main/java/ch/uzh/csg/reimbursement/service/PdfGenerationService.java
+++ src/main/java/ch/uzh/csg/reimbursement/service/PdfGenerationService.java
@@ -30,6 +30,12 @@ import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +46,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 import ch.uzh.csg.reimbursement.application.xml.XmlConverter;
-import ch.uzh.csg.reimbursement.dto.AttachmentCoverPdfDto;
 import ch.uzh.csg.reimbursement.dto.AttachmentPdfDto;
 import ch.uzh.csg.reimbursement.dto.ExpenseItemPdfDto;
 import ch.uzh.csg.reimbursement.dto.ExpensePdfDto;
@@ -76,7 +81,7 @@ public class PdfGenerationService {
 
 	public void generateExpensePdf(String uid, String url) {
 		Expense expense = expenseService.getByUid(uid);
-		if (authorizationService.checkPdfGenerationAuthorization(expense)) {
+		//if (authorizationService.checkPdfGenerationAuthorization(expense)) {
 			String tokenUid = tokenService.createUniAdminToken(uid);
 			String urlWithTokenUid = url + tokenUid;
 			String xslClasspath = "classpath:xml2fo.xsl";
@@ -104,10 +109,10 @@ public class PdfGenerationService {
 			ByteArrayOutputStream pdfConcat = concatPdf(new ByteArrayInputStream(outputStream.toByteArray()), expense);
 			Document doc = new Document(MIME_PDF, pdfConcat.size(), pdfConcat.toByteArray(), GENERATED_PDF);
 			expense.setPdf(doc);
-		} else {
-			LOG.debug("The PDF cannot be generated in this state");
-			throw new PdfGenerationException();
-		}
+		//} else {
+		//	LOG.debug("The PDF cannot be generated in this state");
+		//	throw new PdfGenerationException();
+		//}
 	}
 
 	public Document generateAttachmentPdf(MultipartFile multipartFile) {
@@ -125,19 +130,6 @@ public class PdfGenerationService {
 
 		ByteArrayOutputStream outputStream = generatePdf(dto, xslClasspath);
 		Document doc = new Document(MIME_PDF, outputStream.size(), outputStream.toByteArray(), ATTACHMENT);
-		return doc;
-	}
-
-	private Document getAttachmentCovers(ExpenseItem expenseItem, int iterator) {
-		AttachmentCoverPdfDto dto;
-		String xslClasspath = "classpath:attachmentCoverXml2fo.xsl";
-
-		dto = new AttachmentCoverPdfDto(expenseItem.getDate(), expenseItem.getCostCategory().getName().getDe(),
-				expenseItem.getCostCategory().getAccountNumber(), expenseItem.getExplanation(),
-				expenseItem.getCalculatedAmount(), expenseItem.getProject(), iterator);
-		ByteArrayOutputStream outputStream = generatePdf(dto, xslClasspath);
-		Document doc = new Document(MIME_PDF, outputStream.size(), outputStream.toByteArray(), ATTACHMENT);
-
 		return doc;
 	}
 
@@ -182,7 +174,6 @@ public class PdfGenerationService {
 		MemoryUsageSetting memUsageSetting = MemoryUsageSetting.setupTempFileOnly();
 		PDFMergerUtility mergerUtility = new PDFMergerUtility();
 		byte[] attachmentByteArray = null;
-		byte[] attachmentCoverByteArray = null;
 
 		Set<ExpenseItem> expenseItemList = expense.getExpenseItems();
 
@@ -194,11 +185,9 @@ public class PdfGenerationService {
 		for (ExpenseItem expenseItem : expenseItemList) {
 			if (expenseItem.getAttachment() != null) {
 				attachmentByteArray = expenseItem.getAttachment().getContent();
-				attachmentCoverByteArray = getAttachmentCovers(expenseItem, iterator).getContent();
 
-				// Add cover page
-				source = new ByteArrayInputStream(attachmentCoverByteArray);
-				mergerUtility.addSource(source);
+				// Add receipt numbers on every receipt page
+				attachmentByteArray = addReceiptNumber(attachmentByteArray, iterator);
 
 				// Add respective pdf or graphic
 				source = new ByteArrayInputStream(attachmentByteArray);
@@ -217,6 +206,56 @@ public class PdfGenerationService {
 			LOG.error("PDF could not be concatenated.");
 			throw new PdfConcatException();
 		}
+	}
+
+	private byte[] addReceiptNumber(byte[] attachmentByteArray, int expenseItemId) {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		PDDocument doc = null;
+		float paddingRight = 80;
+		float paddingTop = 40;
+		float paddingBox = 5;
+		float boxWidth = 70;
+		float boxHeight = 13;
+
+		try {
+			doc = PDDocument.load(attachmentByteArray);
+			PDPageTree allPages = doc.getDocumentCatalog().getPages();
+			PDPage page = null;
+
+			for(int i=0; i<allPages.getCount(); i++) {
+				page = allPages.get(i);
+				PDPageContentStream contentStream = new PDPageContentStream(doc, page, true, true, true);
+
+				float documentHeight = page.getMediaBox().getHeight();
+				float documentWidth = page.getMediaBox().getWidth();
+
+				contentStream.setNonStrokingColor(255, 255, 255); // white background
+				contentStream.addRect((documentWidth - paddingRight - paddingBox), (documentHeight - paddingTop - paddingBox), boxWidth, boxHeight);
+				contentStream.fill();
+
+				// Define font used
+				PDFont font = PDType1Font.HELVETICA;
+				float fontSize = 10.0f;
+				contentStream.setFont(font, fontSize);
+				// Define black text
+				contentStream.setNonStrokingColor(0, 0, 0); 
+				contentStream.beginText();
+				contentStream.newLineAtOffset((documentWidth - paddingRight), (documentHeight - paddingTop));
+				contentStream.showText("Beleg Nr: " + expenseItemId);
+				contentStream.endText();
+
+				contentStream.close();
+			}
+
+			doc.save(output);
+			doc.close();
+
+		} catch (IOException e) {
+			LOG.error("PDF could not be generated.");
+			throw new PdfGenerationException();
+		}
+
+		return output.toByteArray();
 	}
 
 	private String generateQRCode(String url) {
